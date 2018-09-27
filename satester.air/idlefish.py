75500903ee8092a5897f39c0a1f0005c0decae52
sa <513969457@qq.com>
2018-09-27 17:03:38 +0800

    init

---
+++ satester.air/idlefish.py
@@ -0,0 +1,24 @@
+# -*- encoding=utf8 -*-
+__author__ = "sa"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+poco = AndroidUiautomationPoco(force_restart=False)
+# start your script here
+
+clear_app("com.taobao.idlefish")
+sleep(1.0)
+
+start_app("com.taobao.idlefish")
+sleep(10.0)
+
+if poco("com.taobao.idlefish:id/pic"):
+    poco("com.taobao.idlefish:id/action_bar_right").click()
+
+sleep(10)
+snapshot(msg="页面显示正常")
+
+clear_app("com.taobao.idlefish")
