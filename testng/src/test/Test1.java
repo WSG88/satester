ff4a1a8b82016c3e9b428394fcfa7abdf8d467ee
wangsigang@hizhu.com <wsghizhu>
2018-10-11 17:04:48 +0800

    整理目录

---
+++ testng/src/test/Test1.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by wsig on 2018-10-11.
+ */
+class Test1 {
+  public static void main(String[] args) {
+    System.out.println("1111111111111111111111111");
+  }
+}
