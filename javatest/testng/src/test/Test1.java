09c1f58be2bb319fc12f3b6fe87dfb02b2588f53
kingdown88 <qc_f741aed4084e49c3b40354525e0bb7b2@git.code.tencent.com>
2018-10-18 18:34:12 +0800

    test

--- javatest/testng/src/test/Test1.java
+++ javatest/testng/src/test/Test1.java
@@ -6,5 +6,7 @@ package test;
 class Test1 {
   public static void main(String[] args) {
     System.out.println("1111111111111111111111111");
+    System.out.println("1111111111111111111111111");
+    System.out.println("1111111111111111111111111");
   }
 }
